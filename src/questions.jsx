0d754ee44daa0b60fcf9f91052fbbd1a0b8eaa8d
Erica Stewart <114890276+esrtewatl@users.noreply.github.com>
2023-04-10 11:14:12 -0400

    need more questions

--- src/questions.jsx
+++ src/questions.jsx
@@ -28,5 +28,192 @@ export const questions = [
     category: 'HTML',
     question: 'What does the <!DOCTYPE html> declaration do?',
     answer: 'The <!DOCTYPE html> declaration is used to tell the browser which version of HTML the document is written in.'
+  },
+  {
+    id: 6 ,
+    category: 'Rest',
+    question:'Describe the advantages of REST web services?',
+    answer: 'I believe that REST provides several advantages that are beneficial to the development process. Such as Scalibility and  Flexibility. It is a lightweight protocol and has a simple learning curve.'
+  },
+  {
+    id: 6 ,
+    category: 'UI',
+    question:'Way to decrease page load times?',
+    answer: 'Image optimization and stylesheet at the top of page,'
+  },
+  {
+    id: 7 ,
+    category: 'Javascript',
+    question:'What is stringify?',
+    answer: 'IN a JSON method, stringify is used to transform a JavaScript object into a string. JSON is a common structure to receive and send data between the web server and the client, and when we wish to send data to the webserver, the object ought to be a string.'
+  },
+  {
+    id: 8 ,
+    category: 'CSS',
+    question:'What are elements of a Box Model?',
+    answer: 'Margin, Border,Padding and Content'
+  },
+  {
+    id: 9 ,
+    category: 'UI',
+    question:'What is progressive rendering?',
+    answer: 'Used to boost pages rendering content by prioritizing visible content for mobile uses and lazy loading of images.'
+  },
+  {
+    id: 10 ,
+    category: 'Mongo',
+    question:'What is MongoDB connection?',
+    answer: 'MongoDB is a NoSQL database that utilizes the JSON-like structure to stow data elements. To modify and access data in MongoDB, the programmer ought to make use of the MongoDB Query Language (MQL)'
+  },
+  {
+    id: 11  ,
+    category: 'Javasript',
+    question:'Define the Anonymous function in JS',
+    answer: 'The function is not defined like a normal function but instead we make use of an assignment operator and variable to stow the function as an object, then utilizing that variable, we will be capable to invoke the function itself'
+  },
+  {
+    id: 12 ,
+    category: 'CSS',
+    question:'What do you know about the CSS image sprites and why it is utilized?',
+    answer: ' In a nutshell, the CSS sprites merge numerous photos into a single large image.'
+  },
+  {
+    id: 13 ,
+    category: 'Dom',
+    question:' What is the difference between attribute and property?',
+    answer: 'Attributes are an element of an HTML document while properties are a part of the Document Object Model (DOM).'
+  },
+  {
+    id: 14 ,
+    category: 'Javascript',
+    question:'State the difference between == and ===?',
+    answer: ' == denotes abstract equality operator, and it inspects if two values are equal or not apart from their data types. Automatically, it transforms the type of both the operands and compares them.'
+  },
+  {
+    id: 15  ,
+    category: 'Javascript',
+    question:'Why do we utilize the “use strict”; statement?',
+    answer: 'The ‘use strict’ statement sets a few restrictions in the script. Typically, it is utilized to facilitate the strict mode of the script, making sure there could be no loose coupling like undeclared variables' 
+  },
+  {
+    id: 16 ,
+    category: 'HTTP',
+    question:'Name the major HTTP requests',
+    answer: 'Get, Head, Trace, Post, Put and Patch ',
   }
+  // {
+  //   id: 17 ,
+  //   category: '',
+  //   question:'',
+  //   answer: '',
+  // }
+  // {
+  //   id:  ,
+  //   category: '',
+  //   question:'',
+  //   answer: '',
+  // }
+  // {
+  //   id:  ,
+  //   category: '',
+  //   question:'',
+  //   answer: '',
+  // }
+  // {
+  //   id:  ,
+  //   category: '',
+  //   question:'',
+  //   answer: '',
+  // }
+  // {
+  //   id:  ,
+  //   category: '',
+  //   question:'',
+  //   answer: '',
+  // }
+  // {
+  //   id:  ,
+  //   category: '',
+  //   question:'',
+  //   answer: '',
+  // }
+  // {
+  //   id:  ,
+  //   category: '',
+  //   question:'',
+  //   answer: '',
+  // }
+  // {
+  //   id:  ,
+  //   category: '',
+  //   question:'',
+  //   answer: '',
+  // }
+  // {
+  //   id:  ,
+  //   category: '',
+  //   question:'',
+  //   answer: '',
+  // }
+  // {
+  //   id:  ,
+  //   category: '',
+  //   question:'',
+  //   answer: '',
+  // }
+  // {
+  //   id:  ,
+  //   category: '',
+  //   question:'',
+  //   answer: '',
+  // }
+  // {
+  //   id:  ,
+  //   category: '',
+  //   question:'',
+  //   answer: '',
+  // }
+  // {
+  //   id:  ,
+  //   category: '',
+  //   question:'',
+  //   answer: '',
+  // }
+  // {
+  //   id:  ,
+  //   category: '',
+  //   question:'',
+  //   answer: '',
+  // }
+  // {
+  //   id:  ,
+  //   category: '',
+  //   question:'',
+  //   answer: '',
+  // }
+  // {
+  //   id:  ,
+  //   category: '',
+  //   question:'',
+  //   answer: '',
+  // }
+  // {
+  //   id:  ,
+  //   category: '',
+  //   question:'',
+  //   answer: '',
+  // }
+  // {
+  //   id:  ,
+  //   category: '',
+  //   question:'',
+  //   answer: '',
+  // }
+  // {
+  //   id:  ,
+  //   category: '',
+  //   question:'',
+  //   answer: '',
+  // }
+
 ];
