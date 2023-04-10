c72daf46ae33fc8eaf955508e399064a23b49c48
Erica Stewart <114890276+esrtewatl@users.noreply.github.com>
2023-04-10 00:29:46 -0400

    begining design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { questions } from './questions';
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [filteredQuestions, setFilteredQuestions] = useState(questions);
+
+  const handleSearch = (event) => {
+    const value = event.target.value;
+    setSearchTerm(value);
+
+    const filtered = questions.filter(question => {
+      const lowerCasedValue = value.toLowerCase();
+      return question.category.toLowerCase().includes(lowerCasedValue) || 
+             question.question.toLowerCase().includes(lowerCasedValue) || 
+             question.answer.toLowerCase().includes(lowerCasedValue);
+    });
+
+    setFilteredQuestions(filtered);
+  };
+
+  const highlightKeyword = (text, keyword) => {
+    const regex = new RegExp(keyword, 'gi');
+    const parts = text.split(regex);
+    return parts.map((part, i) => {
+      return (
+        <span key={i}>
+          {part}
+          {i < parts.length - 1 && <mark>{keyword}</mark>}
+        </span>
+      );
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='body'>
+      <input type="text" placeholder="Search" value={searchTerm} onChange={handleSearch} />
+      {filteredQuestions.map(question => (
+        <div className='questions' key={question.id}>
+          <div className='category'>{question.category}</div>
+          <div className='question'>{highlightKeyword(question.question, searchTerm)}</div>
+          <div className='answer'>{highlightKeyword(question.answer, searchTerm)}</div>
+        </div>
+      ))}
     </div>
   );
 }
---
+++ src/questions.jsx
@@ -0,0 +1,32 @@
+export const questions = [
+  {
+    id: 1,
+    category: 'React',
+    question: 'What is React?',
+    answer: 'React is a JavaScript library for building user interfaces.'
+  },
+  {
+    id: 2,
+    category: 'React',
+    question: 'What are the features of React?',
+    answer: 'The features of React include virtual DOM, JSX, component-based architecture, and unidirectional data flow.'
+  },
+  {
+    id: 3,
+    category: 'JavaScript',
+    question: 'What is a closure in JavaScript?',
+    answer: 'A closure is a function that has access to variables in its outer scope, even after the outer function has returned.'
+  },
+  {
+    id: 4,
+    category: 'JavaScript',
+    question: 'What is the difference between let and var in JavaScript?',
+    answer: 'let is block-scoped, while var is function-scoped.'
+  },
+  {
+    id: 5,
+    category: 'HTML',
+    question: 'What does the <!DOCTYPE html> declaration do?',
+    answer: 'The <!DOCTYPE html> declaration is used to tell the browser which version of HTML the document is written in.'
+  }
+];
